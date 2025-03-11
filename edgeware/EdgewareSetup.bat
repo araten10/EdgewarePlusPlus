fa350ae69a4e3d9e389ea0cbcbe94475d2a6ecdc
LewdDevelopment <158585445+LewdDevelopment@users.noreply.github.com>
2025-03-11 10:17:03 +0200

    Refactor Windows setup script

--- edgeware/EdgewareSetup.bat
+++ edgeware/EdgewareSetup.bat
@@ -1,47 +1,57 @@
 @echo off
-:open
-echo +==============[ Welcome to Edgeware Setup~ ]==============+
+
+echo +==============[ Welcome to Edgeware++ Setup~ ]==============+
 echo Python version:
 py --version
 echo:
 echo NOTE: Python versions older than 3.12 might have compatability issues.
-echo If you are on one of these versions and experience issues with Edgeware, try uninstalling them
+echo If you are on one of these versions and experience issues with Edgeware++, try uninstalling them
 echo and running this installer again. (or download it yourself if you know what you're doing!)
 echo:
-if NOT %errorlevel%==0 goto pyInstall
-goto checkPip
-:pyInstall
-echo Could not find Python.
-echo Now downloading installer from python.org, please wait...
-reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
-if %OS%==32BIT curl https://www.python.org/ftp/python/3.12.6/python-3.12.6.exe -o pyinstaller.exe
-if %OS%==64BIT curl https://www.python.org/ftp/python/3.12.6/python-3.12.6-amd64.exe -o pyinstaller.exe
-echo Done downloading executable.
-echo Please complete installation through the installer before continuing.
-start %CD%\pyinstaller.exe
-pause
-:verifyInstallation
-py --version
-if NOT %errorlevel%==0 goto quitPy
-goto checkPip
-:checkPip
+
+if not %errorlevel%==0 (
+  echo Could not find Python.
+  echo Now downloading installer from python.org, please wait...
+
+  reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
+  if %OS%==32BIT curl https://www.python.org/ftp/python/3.12.6/python-3.12.6.exe -o pyinstaller.exe
+  if %OS%==64BIT curl https://www.python.org/ftp/python/3.12.6/python-3.12.6-amd64.exe -o pyinstaller.exe
+
+  echo Done downloading executable.
+  echo Please complete installation through the installer before continuing.
+  start %CD%\pyinstaller.exe
+  pause
+
+  py --version
+  if not %errorlevel%==0 (
+    echo Python still could not be found.
+    pause
+    exit
+  )
+)
+
 echo pip version:
 py -m pip --version
-if NOT %errorlevel%==0 goto installPip
-goto requirements
-:installPip
-echo Could not find pip.
-echo Installing pip with ensurepip...
-py -m ensurepip --upgrade
-py -m pip --version
-if NOT %errorlevel%==0 goto quitPip
-goto requirements
-:requirements
+if not %errorlevel%==0 (
+  echo Could not find pip.
+  echo Installing pip with ensurepip...
+  py -m ensurepip --upgrade
+  py -m pip --version
+  if NOT %errorlevel%==0 (
+    echo pip still could not be found.
+    pause
+    exit
+  )
+)
+
 echo Installing requirements...
 py -m pip install -r requirements.txt
-if NOT %errorlevel%==0 goto quitRequirements
-goto mpv
-:mpv
+if not %errorlevel%==0 (
+  echo Failed to install requirements.
+  pause
+  exit
+)
+
 if not exist data\libmpv-2.dll (
   echo Installing libmpv...
   if not exist data mkdir data
@@ -53,12 +63,12 @@ if not exist data\libmpv-2.dll (
   )
   data\7z.exe e data\mpv.7z -odata libmpv-2.dll
 )
-goto shortcuts
-:shortcuts
+
 call :makePyw "CONFIG" "config"
 call :makePyw "MAIN" "edgeware"
 call :makePyw "PANIC" "panic"
 goto run
+
 :makePyw
 (
   @echo import subprocess
@@ -67,22 +77,14 @@ goto run
   @echo subprocess.run^([sys.executable, Process.%~1]^)
 ) > %~2.pyw
 goto :eof
+
 :run
-echo Edgeware is ready, and will now start the config file for you.
+echo Edgeware++ is ready, and will now start the config file for you.
 echo:
-echo For first time users, here are the files you'll want to run to use Edgeware in the future:
-echo config.pyw: runs the config window which allows changing Edgeware settings
-echo edgeware.pyw: starts Edgeware with the config settings you have saved
-echo panic.pyw: kills Edgeware and all currently spawned popups
+echo For first time users, here are the files you'll want to run to use Edgeware++ in the future:
+echo config.pyw: runs the config window which allows changing Edgeware++ settings
+echo edgeware.pyw: starts Edgeware++ with the config settings you have saved
+echo panic.pyw: kills Edgeware++ and all currently spawned popups
 pause
 start "Edgeware++ Config" "%CD%/config.pyw"
 exit
-:quitPy
-echo Python still could not be found.
-pause
-:quitPip
-echo pip still could not be found.
-pause
-:quitRequirements
-echo Failed to install requirements.
-pause
