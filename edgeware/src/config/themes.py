2d308acaeed68dad1284089ec974df45306122a0
LewdDevelopment <158585445+LewdDevelopment@users.noreply.github.com>
2025-06-01 11:07:22 +0300

    Merge all theme colors

--- edgeware/src/config/themes.py
+++ edgeware/src/config/themes.py
@@ -27,6 +27,12 @@ class Theme:
     fg: str
     bg: str
     active_bg: str
+    disabled_bg: str
+    button_fg: str
+    tab_bg: str
+    tab_frame_bg: str
+    trough: str
+    select: str
     text_fg: str
     text_bg: str
     font: (str, int)
@@ -34,19 +40,91 @@ class Theme:
 
 
 THEMES = {
-    "Original": Theme(fg="black", bg="#d9d9d9", active_bg="#ececec", text_fg="black", text_bg="white", font=("TkDefaultFont", 9), transparent_bg="#000001"),
-    "Dark": Theme(
-        fg="ghost white", bg="#282c34", active_bg="#282c34", text_fg="ghost white", text_bg="#1b1d23", font=("TkDefaultFont", 9), transparent_bg="#f9fafe"
+    "Original": Theme(
+        fg="black",
+        bg="#d9d9d9",
+        active_bg="#ececec",
+        disabled_bg="gray35",
+        button_fg="black",
+        tab_bg="#d9d9d9",
+        tab_frame_bg="#f0f0f0",
+        trough="#b3b3b3",
+        select="#ffffff",
+        text_fg="black",
+        text_bg="white",
+        font=("TkDefaultFont", 9),
+        transparent_bg="#000001",
     ),
-    "The One": Theme(fg="#00ff41", bg="#282c34", active_bg="#1b1d23", text_fg="#00ff41", text_bg="#1b1d23", font=("Consolas", 9), transparent_bg="#00ff42"),
-    "Ransom": Theme(fg="white", bg="#841212", active_bg="#841212", text_fg="black", text_bg="white", font=("Arial Bold", 9), transparent_bg="#fffffe"),
+    "Dark": Theme(
+        fg="ghost white",
+        bg="#282c34",
+        active_bg="#282c34",
+        disabled_bg="gray65",
+        button_fg="ghost white",
+        tab_bg="#1b1d23",
+        tab_frame_bg="#282c34",
+        trough="#c8c8c8",
+        select="#1b1d23",
+        text_fg="ghost white",
+        text_bg="#1b1d23",
+        font=("TkDefaultFont", 9),
+        transparent_bg="#f9fafe",
+    ),
+    "The One": Theme(
+        fg="#00ff41",
+        bg="#282c34",
+        active_bg="#1b1d23",
+        disabled_bg="#37573d",
+        button_fg="#00ff41",
+        tab_bg="#1b1d23",
+        tab_frame_bg="#282c34",
+        trough="#009a22",
+        select="#1b1d23",
+        text_fg="#00ff41",
+        text_bg="#1b1d23",
+        font=("Consolas", 9),
+        transparent_bg="#00ff42",
+    ),
+    "Ransom": Theme(
+        fg="white",
+        bg="#841212",
+        active_bg="#841212",
+        disabled_bg="573737",
+        button_fg="yellow",
+        tab_bg="#5c0d0d",
+        tab_frame_bg="#841212",
+        trough="#c8c8c8",
+        select="#5c0d0d",
+        text_fg="black",
+        text_bg="white",
+        font=("Arial Bold", 9),
+        transparent_bg="#fffffe",
+    ),
     "Goth": Theme(
-        fg="MediumPurple1", bg="#282c34", active_bg="#282c34", text_fg="purple4", text_bg="MediumOrchid2", font=("Constantia", 9), transparent_bg="#ba9afe"
+        fg="MediumPurple1",
+        bg="#282c34",
+        active_bg="#282c34",
+        disabled_bg="#4b3757",
+        button_fg="MediumPurple1",
+        tab_bg="#1b1d23",
+        tab_frame_bg="#282c34",
+        trough="MediumOrchid2",
+        select="#1b1d23",
+        text_fg="purple4",
+        text_bg="MediumOrchid2",
+        font=("Constantia", 9),
+        transparent_bg="#ba9afe",
     ),
     "Bimbo": Theme(
         fg="deep pink",
         bg="pink",
         active_bg="hot pink",
+        disabled_bg="#bc7abf",
+        button_fg="deep pink",
+        tab_bg="light pink",
+        tab_frame_bg="pink",
+        trough="hot pink",
+        select="light pink",
         text_fg="magenta2",
         text_bg="light pink",
         font=("Constantia", 9),
@@ -59,82 +137,40 @@ THEMES = {
 
 CONFIG_THEMES = {
     "Original": {
-        "bg-disabled": "gray35",
-        "background": "#f0f0f0",
-        "Button-fg": "black",  # Added
-        "troughcolor": "#b3b3b3",  # Added
-        "selectcolor": "#ffffff",  # Added
         "Message-font": ("TkDefaultFont", 8),
         "m-family": "TkDefaultFont",  # Added
         "m-size": 10,  # Added
         "t-family": "TkDefaultFont",  # Added
-        "Tab-background": "#d9d9d9",
-        "Tab-foreground": "black",  # Added
     },
     "Dark": {
-        "bg-disabled": "gray65",
-        "background": "#282c34",
-        "Button-fg": "ghost white",
-        "troughcolor": "#c8c8c8",
-        "selectcolor": "#1b1d23",
         "Message-font": ("TkDefaultFont", 8),
         "m-family": "TkDefaultFont",  # Added
         "m-size": 10,  # Added
         "t-family": "TkDefaultFont",  # Added
-        "Tab-background": "#1b1d23",
-        "Tab-foreground": "#f9faff",
     },
     "The One": {
-        "bg-disabled": "#37573d",
-        "background": "#282c34",
-        "Button-fg": "#00ff41",
-        "troughcolor": "#009a22",
-        "selectcolor": "#1b1d23",
         "Message-font": ("Consolas", 8),
         "m-family": "Consolas",
         "m-size": 8,
         "t-family": "Consolas",
-        "Tab-background": "#1b1d23",
-        "Tab-foreground": "#00ff41",
     },
     "Ransom": {
-        "bg-disabled": "573737",
-        "background": "#841212",
-        "Button-fg": "yellow",
-        "troughcolor": "#c8c8c8",
-        "selectcolor": "#5c0d0d",
         "Message-font": ("Arial", 8),
         "m-family": "Arial",
         "m-size": 10,  # Added
         "t-family": "Arial Bold",
-        "Tab-background": "#5c0d0d",
-        "Tab-foreground": "#ffffff",
     },
     "Goth": {
-        "bg-disabled": "#4b3757",
-        "background": "#282c34",
-        "Button-fg": "MediumPurple1",
-        "troughcolor": "MediumOrchid2",
-        "selectcolor": "#1b1d23",
         "Message-font": ("Constantia", 8),
         "m-family": "Constantia",
         "m-size": 10,  # Added
         "t-family": "Constantia",
-        "Tab-background": "#1b1d23",
-        "Tab-foreground": "MediumPurple1",
     },
     "Bimbo": {
-        "bg-disabled": "#bc7abf",
-        "background": "pink",
-        "Button-fg": "deep pink",
-        "troughcolor": "hot pink",
-        "selectcolor": "light pink",
         "Message-font": ("Constantia", 8),
         "m-family": "Constantia",
         "m-size": 10,  # Added
         "t-family": "Constantia",
-        "Tab-background": "light pink",
-        "Tab-foreground": "deep pink",
     },
 }
 
@@ -149,7 +185,7 @@ def theme_change(name: str, root: Misc, style: ttk.Style, mfont: Font, tfont: Fo
         if isinstance(widget, Frame) or isinstance(widget, Canvas):
             widget.configure(bg=theme.bg)
         if isinstance(widget, Button):
-            widget.configure(bg=theme.bg, fg=t["Button-fg"], activebackground=theme.active_bg, activeforeground=theme.fg)
+            widget.configure(bg=theme.bg, fg=theme.button_fg, activebackground=theme.active_bg, activeforeground=theme.fg)
         if isinstance(widget, Label):
             if not hasattr(widget, "ignore_theme_fg"):
                 widget.configure(fg=theme.fg)
@@ -160,13 +196,13 @@ def theme_change(name: str, root: Misc, style: ttk.Style, mfont: Font, tfont: Fo
         if isinstance(widget, Text):
             widget.configure(bg=theme.text_bg, fg=theme.text_fg)
         if isinstance(widget, Scale):
-            widget.configure(bg=theme.bg, fg=theme.fg, activebackground=theme.bg, troughcolor=t["troughcolor"], highlightthickness=0)
+            widget.configure(bg=theme.bg, fg=theme.fg, activebackground=theme.bg, troughcolor=theme.trough, highlightthickness=0)
         if isinstance(widget, Checkbutton):
             # activebackground was "bg" but "Button-activebackground" is true color for default theme
             widget.configure(
                 bg=theme.bg,
                 fg=theme.fg,
-                selectcolor=t["selectcolor"],
+                selectcolor=theme.select,
                 activebackground=theme.active_bg,
                 activeforeground=theme.fg,
                 highlightthickness=0,
@@ -174,17 +210,17 @@ def theme_change(name: str, root: Misc, style: ttk.Style, mfont: Font, tfont: Fo
         if isinstance(widget, Message):
             widget.configure(bg=theme.bg, fg=theme.fg, font=t["Message-font"])
         if isinstance(widget, HtmlFrame):
-            widget.add_css(f"html{{background: {t['bg']}; color: {t['fg']}; font-family: {t['m-family']};}}")
-    style.configure("TFrame", background=t["background"])
-    style.configure("TNotebook", background=t["background"])
-    style.map("TNotebook.Tab", background=[("selected", t["background"])])
-    style.configure("TNotebook.Tab", background=t["Tab-background"], foreground=t["Tab-foreground"])
+            widget.add_css(f"html{{background: {theme.bg}; color: {theme.fg}; font-family: {t['m-family']};}}")
+    style.configure("TFrame", background=theme.tab_frame_bg)
+    style.configure("TNotebook", background=theme.tab_frame_bg)
+    style.map("TNotebook.Tab", background=[("selected", theme.tab_frame_bg)])
+    style.configure("TNotebook.Tab", background=theme.tab_bg, foreground=theme.fg)
     mfont.configure(family=t["m-family"], size=t["m-size"])
     tfont.configure(family=t["t-family"])
 
     for widget in all_children(root):
         try:
             if widget["state"] == "disabled":
-                widget.configure(bg=t["bg-disabled"])
+                widget.configure(bg=theme.disabled_bg)
         except TclError:
             pass
