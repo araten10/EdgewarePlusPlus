e7e5b627eef7c741808aab73f0d1b60c97390c17
araten10 <141581742+araten10@users.noreply.github.com>
2025-06-25 04:59:17 -0400

    commenting out fade temporarily due to bug

--- edgeware/src/main_edgeware.py
+++ edgeware/src/main_edgeware.py
@@ -117,5 +117,5 @@ if __name__ == "__main__":
     else:
         start_main()
 
-    Thread(target=pyglet.app.run, daemon=True).start()  # Required for pyglet events
+    #Thread(target=pyglet.app.run, daemon=True).start()  # Required for pyglet events
     root.mainloop()
